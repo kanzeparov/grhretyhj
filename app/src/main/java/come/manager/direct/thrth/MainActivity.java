b9a3539abab4b14205cea956463be7d2271c45fd
Kanzeparov <kanzeparov@yandex.ru>
2019-01-12 10:29:58 +0300

    xtdfyt

--- app/src/main/java/come/manager/direct/thrth/MainActivity.java
+++ app/src/main/java/come/manager/direct/thrth/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int a = 5;
+        int b =5;
         setContentView(R.layout.activity_main);
     }
 }
